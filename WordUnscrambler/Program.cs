8003dffa6bdbe8b8c9185b35d66a1d56b9fbe87d
Patrick <revmydev@gmail.com>
2019-07-10 17:46:56 -0700

    structural bits I missed last  commit

--- WordUnscrambler/Program.cs
+++ WordUnscrambler/Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using WordUnscrambler.Data;
+using WordUnscrambler.Workers;
 
 namespace WordUnscrambler
 {
     class Program
     {
+        private static readonly FileReader _fileReader = new FileReader();
+        private static readonly WordMatcher _wordMatcher = new WordMatcher();
+        private const string wordListFileName = "wordlist.txt";
+
         static void Main(string[] args)
         {
             bool continueWordUnscramble = true;
@@ -32,27 +36,50 @@ namespace WordUnscrambler
                         break;
                 }
 
-                string continueUnscrambledWordDecision = string.Empty;
+                string continueDecision = string.Empty;
                 do
                 {
                     Console.Write("Do you want to continue? [y/n]: ");
-                    continueUnscrambledWordDecision = (Console.ReadLine() ?? string.Empty);
+                    continueDecision = (Console.ReadLine() ?? string.Empty);
                 } while (
-                    !continueUnscrambledWordDecision.Equals("Y", StringComparison.OrdinalIgnoreCase) && 
-                    !continueUnscrambledWordDecision.Equals("N", StringComparison.OrdinalIgnoreCase));
+                    !continueDecision.Equals("Y", StringComparison.OrdinalIgnoreCase) && 
+                    !continueDecision.Equals("N", StringComparison.OrdinalIgnoreCase));
 
-                continueWordUnscramble = continueUnscrambledWordDecision.Equals("Y", StringComparison.OrdinalIgnoreCase);
+                continueWordUnscramble = continueDecision.Equals("Y", StringComparison.OrdinalIgnoreCase);
             } while (continueWordUnscramble);
         }
 
         private static void ExecuteScrambledWordsManualEntryScenario()
         {
-
+            var manualInput = Console.ReadLine() ?? string.Empty;
+            string[] scrambledWords = manualInput.Split(',');
+            DisplayMatchedUnscrambledWords(scrambledWords);
         }
 
         private static void ExecuteScrambledWordsInFileScenario()
         {
-            
+            var filename = Console.ReadLine() ?? string.Empty;
+            string[] scrambledWords = _fileReader.Read(filename);
+            DisplayMatchedUnscrambledWords(scrambledWords);
+        }
+
+        private static void DisplayMatchedUnscrambledWords(string[] scrambledWords)
+        {
+            string[] wordList = _fileReader.Read(wordListFileName);
+
+            List<MatchedWord> matchedWords = _wordMatcher.Match(scrambledWords, wordList);
+
+            if (matchedWords.Any())
+            {
+                foreach(var matchedWord in matchedWords)
+                {
+                    Console.WriteLine("Match found for {0}: {1}", matchedWord.ScrambledWord, matchedWord.Word);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No matches found.");
+            }
         }
     }
 }
---
+++ WordUnscrambler/Workers/FileReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordUnscrambler.Workers
+{
+    public class FileReader
+    {
+        internal string[] Read(string filename)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
