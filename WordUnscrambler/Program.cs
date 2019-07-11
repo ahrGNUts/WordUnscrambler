a41c21aa583513cf8d03365ad4654fdd06675110
Patrick <revmydev@gmail.com>
2019-07-10 17:09:09 -0700

    basic program structure

--- WordUnscrambler/Program.cs
+++ WordUnscrambler/Program.cs
@@ -10,6 +10,49 @@ namespace WordUnscrambler
     {
         static void Main(string[] args)
         {
+            bool continueWordUnscramble = true;
+
+            do
+            {
+                Console.WriteLine("Please enter option - F for file or M for manual");
+                var option = Console.ReadLine() ?? string.Empty;
+
+                switch (option.ToUpper())
+                {
+                    case "F":
+                        Console.Write("Enter the scrambled words filename: ");
+                        ExecuteScrambledWordsInFileScenario();
+                        break;
+                    case "M":
+                        Console.Write("Enter scrambled words manually: ");
+                        ExecuteScrambledWordsManualEntryScenario();
+                        break;
+                    default:
+                        Console.Write("Option not recognized.");
+                        break;
+                }
+
+                string continueUnscrambledWordDecision = string.Empty;
+                do
+                {
+                    Console.Write("Do you want to continue? [y/n]: ");
+                    continueUnscrambledWordDecision = (Console.ReadLine() ?? string.Empty);
+                } while (
+                    !continueUnscrambledWordDecision.Equals("Y", StringComparison.OrdinalIgnoreCase) && 
+                    !continueUnscrambledWordDecision.Equals("N", StringComparison.OrdinalIgnoreCase));
+
+                continueWordUnscramble = continueUnscrambledWordDecision.Equals("Y", StringComparison.OrdinalIgnoreCase);
+            } while (continueWordUnscramble);
+        }
+
+        private static void ExecuteScrambledWordsManualEntryScenario()
+        {
+
+        }
+
+        private static void ExecuteScrambledWordsInFileScenario()
+        {
+            
         }
     }
 }
