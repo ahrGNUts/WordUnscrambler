fd71aa0b57d2e9c715de624ef2c48eb5c59d888d
Patrick <revmydev@gmail.com>
2019-07-10 17:46:19 -0700

    more structural bits

---
+++ WordUnscrambler/Workers/WordMatcher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using WordUnscrambler.Data;
+
+namespace WordUnscrambler.Workers
+{
+    public class WordMatcher
+    {
+        internal System.Collections.Generic.List<MatchedWord> Match(string[] scrambledWords, string[] wordList)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
